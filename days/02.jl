a3b9c59cef001f3a4a9b2eb39fb0816ec75041a8
Andrew Hynes <andrew.hynes@colabsoftware.com>
2022-12-04 16:17:58 -0500

    refactor: read file by line in day 02

--- days/02.jl
+++ days/02.jl
@@ -3,7 +3,7 @@ using DataFrames
 
 victor_of = Dict('A' => 'B', 'B' => 'C', 'C' => 'A')
 
-function outcome_score(move_1, move_2)
+function compute_outcome_score(move_1, move_2)
     if move_1 == move_2
         return 3
     end
@@ -14,24 +14,13 @@ function outcome_score(move_1, move_2)
 end
 
 
-function game_score(move_1, move_2)
+function compute_game_score(move_1, move_2)
     shape_score = Dict('A' => 1, 'B' => 2, 'C' => 3)
-    return shape_score[move_1] + outcome_score(move_1, move_2)
-end
-
-
-function compute_total_score(df)
-    strategy_map = Dict('X' => 'A', 'Y' => 'B', 'Z' => 'C')
-    df.strategy = map(x -> strategy_map[x], df.strategy)
-
-    df[!, "total_score"] = game_score.(df.strategy, df.opponent)
-
-    return sum(df.total_score)
+    return shape_score[move_1] + compute_outcome_score(move_1, move_2)
 end
 
 
 function decide_move_of_part_2(opponent, strategy)
-
     loser_of = Dict(victor_of[k] => k for k in keys(victor_of))
 
     if strategy == 'X'
@@ -46,24 +35,32 @@ function decide_move_of_part_2(opponent, strategy)
     else
         error(strategy)
     end
+
     return move
 end
 
-function compute_total_score_of_part_2(df)
-    moves = decide_move_of_part_2.(df.opponent, df.strategy)
-    df[!, "total_score"] = game_score.(moves, df.opponent)
 
-    return sum(df.total_score)
+function compute_total_scores(file)
+    strategy_map = Dict('X' => 'A', 'Y' => 'B', 'Z' => 'C')
+    total_score_1, total_score_2 = 0, 0
+
+    for line in readlines(file)
+        opponent_move, strategy = only.(split(line, ' '))
+
+        my_move_1 = strategy_map[strategy]
+        my_move_2 = decide_move_of_part_2(opponent_move, strategy)
+
+        total_score_1 += compute_game_score(my_move_1, opponent_move)
+        total_score_2 += compute_game_score(my_move_2, opponent_move)
+    end
+
+    return total_score_1, total_score_2
 end
 
-df = DataFrame(readdlm("data/02.txt", header=false), :auto)
 
-rename!(df, [:opponent, :strategy])
+file = open("data/02.txt")
 
-# Convert elements from string to char
-df = only.(df)
-# total_score = compute_total_score(df)
-# println(total_score)
+total_score_1, total_score_2 = compute_total_scores(file)
 
-total_score_2 = compute_total_score_of_part_2(df)
-println(total_score_2)
+println("Part 1: ", total_score_1)
+println("Part 2: ", total_score_2)
