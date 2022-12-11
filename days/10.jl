31b1d3a9e038226a6415e8fee7940c710ac75794
Andrew Hynes <andrew.hynes@colabsoftware.com>
2022-12-11 00:41:53 -0500

    feat: day 10

---
+++ days/10.jl
@@ -0,0 +1,64 @@
+using Plots
+
+
+function execute_program(file)
+    cycle = 0
+    x = 1
+
+    strengths = Int[]
+    crt = fill(false, 6, 40)
+
+    for line in readlines(file)
+        if line != "noop"
+            words = split(line, ' ')
+
+            for _ in 1:2
+                cycle += 1
+                update_strengths!(strengths, cycle, x)
+                update_crt!(crt, cycle, x)
+            end
+
+            x += parse(Int, words[2])
+        else
+            cycle += 1
+            update_strengths!(strengths, cycle, x)
+            update_crt!(crt, cycle, x)
+        end
+    end
+
+    strengths, crt
+end
+
+
+function update_strengths!(strengths, cycle, x)
+    if (cycle - 20) % 40 == 0
+        push!(strengths, cycle * x)
+    end
+end
+
+
+function update_crt!(crt, cycle, x)
+    _, width = size(crt)
+
+    index = cycle - 1
+    row, column = get_row_and_column(width, index)
+
+    if abs(column - x) <= 1
+        crt[row+1, column+1] = true
+    end
+end
+
+
+function get_row_and_column(width, index)
+    row = floor(Int, index / width)
+    column = index % width
+
+    row, column
+end
+
+
+file = open("data/10.txt")
+strengths, crt = execute_program(file)
+
+println("Part 1: ", sum(strengths))
+display(plot(Gray.(.!crt)))
