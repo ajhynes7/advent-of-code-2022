d75ab9815e7aabf6842d0788c3f2b42a29a8687f
Andrew Hynes <andrew.hynes@colabsoftware.com>
2022-12-16 21:21:57 -0330

    refactor: split functions more

--- days/12.jl
+++ days/12.jl
@@ -19,18 +19,8 @@ function get_index(i, j, n_cols)
 end
 
 
-function shortest_number_of_steps(file)
-    grid = construct_grid(file)
+function construct_graph(grid)
 
-    index_start = findall(x -> x == 'S', grid)[1]
-    index_end = findall(x -> x == 'E', grid)[1]
-
-    grid[index_start] = 'a'
-    grid[index_end] = 'z' 
-
-    grid = transpose(Int.(grid))
-
-    n_rows, n_cols = size(grid)
     padded = padarray(grid, Pad(:replicate, 1, 1))
 
     padded[:, 0] .= 1000
@@ -44,9 +34,11 @@ function shortest_number_of_steps(file)
        add_vertex!(graph) 
     end
 
+    n_rows, n_cols = size(grid)
 
     for i = 1:n_rows
         for j = 1:n_cols
+
             node_u = get_index(i, j, n_cols)  
 
             if padded[i-1, j] - padded[i, j] <= 1
@@ -68,15 +60,33 @@ function shortest_number_of_steps(file)
                 node_v = get_index(i, j+1, n_cols) 
                 add_edge!(graph, node_u+1, node_v+1)
             end
-
         end
     end
 
-    source = get_index(index_start[2], index_start[1], n_cols)
-    target = get_index(index_end[2], index_end[1], n_cols)
+    graph
+end
 
-    result = dijkstra_shortest_paths(graph, source+1)
-    result.dists[target+1]
+
+function shortest_number_of_steps(file)
+
+    grid = construct_grid(file)
+
+    index_start = findall(x -> x == 'S', grid)[1]
+    index_end = findall(x -> x == 'E', grid)[1]
+
+    grid[index_start] = 'a'
+    grid[index_end] = 'z' 
+
+    grid = transpose(Int.(grid))
+
+    graph = construct_graph(grid)
+
+    n_cols = size(grid)[2]
+    source = get_index(index_start[2], index_start[1], n_cols) + 1
+    target = get_index(index_end[2], index_end[1], n_cols) + 1
+
+    result = dijkstra_shortest_paths(graph, source)
+    result.dists[target]
 end
 
 
