a40ccd01298348923436928ec0aad4a1e051cac1
Andrew Hynes <andrew.hynes@colabsoftware.com>
2022-12-17 01:33:44 -0330

    feat: day 13 - part 1 - draft 2

--- days/13.jl
+++ days/13.jl
@@ -5,13 +5,13 @@ function read_packets(file)
     packet_pairs = []
 
     for line in readlines(file)
-        
+
         if isempty(line)
             push!(packet_pairs, pair)
             pair = []
             continue
         end
-        
+
         vector = Meta.parse(line) |> eval
         push!(pair, vector)
     end
@@ -21,41 +21,56 @@ end
 
 
 function in_order(left, right)
-    
-    if left isa Int && right isa Int
-        return left < right
-        
-    elseif left isa Array && right isa Int
-        for x in left
-            return in_order(x, right)
-        end
-        
-    elseif left isa Int && right isa Array
-        for x in right
-            return in_order(left, x)
-        end
-        
-    else
-        if length(left) > length(right)
-            return false
-        end
-        for (x, y) in zip(left, right)
-            if x == y
-                continue
+
+    if left isa Vector && right isa Vector
+        index = 1
+        length_l = length(left)
+        length_r = length(right)
+
+        while true
+            if index > length_l
+                # Left ran out of items first.
+                return true
             end
-            ordered = in_order(x, y)
-            if !ordered
+            if index > length_r
+                # Right ran out of items first.
                 return false
             end
+
+            ordered = in_order(left[index], right[index])
+
+            if !isnothing(ordered)
+                return ordered
+            end
+
+            index += 1
         end
+
         return true
     end
-    
+
+    if left isa Int && right isa Int
+
+        if left == right
+            return
+        end
+
+        return left < right
+    end
+
+    if left isa Vector && right isa Int
+        return in_order(left, [right])
+    end
+
+    if left isa Int && right isa Vector
+        return in_order([left], right)
+    end
+
 end
 
 
 function compute_total(packet_pairs)
-    total = 0 
+    total = 0
 
     for (i, pair) in enumerate(packet_pairs)
         if in_order(pair...)
@@ -67,7 +82,7 @@ function compute_total(packet_pairs)
 end
 
 
-file = open("data/13_prac.txt")
+file = open("data/13.txt")
 packet_pairs = read_packets(file)
 
 println("Part 1: ", compute_total(packet_pairs))
